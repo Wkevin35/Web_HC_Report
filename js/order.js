152f622bafa875185b06a1cf042db2180f47995a
kevin <j309028133@gmail.com>
2020-07-12 17:09:29 +0800

    feat():修改js

--- js/order.js
+++ js/order.js
@@ -1,17 +1,22 @@
-var index = 0;
-var maxIndex = 1;
+let index = 0;
+let maxIndex = 1;
 
-var item0 = [{
+let item = [];
+
+const noodels = [
+   {
     id: 1,
     title: "粗麵",
     src: "./style/img/noodle1.png"
-}, {
+   }, 
+   {
     id:2,
     title: "細麵",
     src: "./style/img/noodle2.png"
-}];
+   }
+];
 
-var item1 = [
+const soup = [
     {
         id: 11,
         title: "味增湯頭",
@@ -20,30 +25,51 @@ var item1 = [
     {
         id: 12,
         title: "醬油湯頭",
-        src: "./style/img/logo.png"
+        src: "./style/img/soy_source.jpg"
+    }
+];
+
+const burden = [
+    {
+        id: 21,
+        title: "叉燒",
+        src: ""
+    },
+    {
+        id: 22,
+        title: "塘心蛋",
+        src: ""
     }
 ];
 
 $(document).ready(function() {
-    item0.forEach(element => {
+    noodels.forEach(element => {
         creatElement(element);
     });
+    
+    // $("#banner-left").css("right", 100);
 });
 
 function onClick(event, id) {
+    const tempIndex = chooseItem(index);
+    if (index <= maxIndex) {
+        item.push(tempIndex.find(x=> {return x.id === id}));
+        console.log(item);
+    }
     ++index;
-    
+
     if (index > maxIndex) {
         return;
     }
-
-    const myItem = chooseItem();
+   
+    const myItem = chooseItem(index);
 
 
     $("#choose").children().remove();
     myItem.forEach(element => {
         creatElement(element);
     });
+
 }
 
 function creatElement(element) {
@@ -59,13 +85,19 @@ function creatElement(element) {
     });
 }
 
-function chooseItem() {
-    switch(index) {
+function chooseItem(localIndex) {
+    switch(localIndex) {
       case 0:
-           return item0;
+           return noodels;
+
       case 1:
-           return item1;     
+           return soup;
+
+      case 2:
+           return burden;   
+
       default:
-           return item0;  
+           return noodels;  
+
     }
 }
